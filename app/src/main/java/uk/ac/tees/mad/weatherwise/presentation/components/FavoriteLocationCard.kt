0155f30eb8268d786b4fa3d4c77c8c658142da6c
Avinash <weatherwise572@gmail.com>
2025-03-18 12:00:00 +0530

    Adding favorite screen and UI

---
+++ app/src/main/java/uk/ac/tees/mad/weatherwise/presentation/components/FavoriteLocationCard.kt
@@ -0,0 +1,114 @@
+package uk.ac.tees.mad.weatherwise.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.abs
+import kotlin.math.roundToInt
+
+@Composable
+fun FavoriteLocationCard(
+    city: String,
+    countryCode: String,
+    weather: String,
+    temperature: String,
+    weatherIcon: ImageVector,
+    onDelete: () -> Unit
+) {
+    var offsetX by remember { mutableFloatStateOf(0f) }
+
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xBA5FEAE5)
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .offset { IntOffset(offsetX.roundToInt(), 0) }
+            .pointerInput(Unit) {
+                detectHorizontalDragGestures(
+                    onDragEnd = {
+                        if (abs(offsetX) > 500) {
+
+                            onDelete()
+                        }
+                        offsetX = 0f
+                    }
+                ) { change, dragAmount ->
+                    offsetX += dragAmount
+                    change.consume()
+                }
+            },
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Text(
+                    text = "$city, $countryCode",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+                Text(
+                    text = weather,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color.White.copy(alpha = 0.8f)
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(
+                    imageVector = weatherIcon,
+                    contentDescription = "Weather Icon",
+                    tint = Color.White,
+                    modifier = Modifier.size(36.dp)
+                )
+
+                Text(
+                    text = "$temperature°C",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/uk/ac/tees/mad/weatherwise/presentation/ui/FavoriteScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/weatherwise/presentation/ui/FavoriteScreen.kt
@@ -1,18 +1,47 @@
 package uk.ac.tees.mad.weatherwise.presentation.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import uk.ac.tees.mad.weatherwise.presentation.components.FavoriteLocationCard
 
 @Composable
 fun FavoriteScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
     Box(modifier=modifier
         .fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
-        Text("Favorite Screen")
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text("Favorite Places",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(16.dp)
+            )
+            LazyColumn {
+                items(10){
+                    FavoriteLocationCard(
+                        "Patna",
+                        "IND",
+                        "Cold",
+                        "22.34",
+                        Icons.Default.Star
+                    ) {
+                        Toast.makeText(context,"Deleted", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
